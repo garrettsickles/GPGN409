34789182042440ecc5ce54951c524f4b254d201c
Garrett Frank Sickles <garrettsickles@gmail.com>
2016-01-27 17:31:00 -0700

    Homework 1 is working but not final

--- Homework/HW1/HW1.m
+++ Homework/HW1/HW1.m
@@ -9,18 +9,22 @@ function HW1(filename)
     y = m(1).*ones(1, length(x)) + m(2).*x + m(3).*(x.^2);
     plot(x, y);
     h0 = r(2);
-    z0 = m(1)+(m(3).*(h0).^2)+(m(2).*h0);
     theta = atan(m(2)+(2.*m(3).*h0));
-    vel = ((-9.81)./((2.0).*m(3).*(cos(theta).^2))).^0.5;
-    display(['Z initial (Km): ', num2str(z0)]);
-    display(['Theta (deg): ', num2str(theta.*180./(2.*3.1415926))]);
+    vel = ((-9.81*(10^(-3)))./((2.0).*m(3).*(cos(theta).^2))).^0.5;
+    display(['X initial (Km): ', num2str(x0)]);
+    display(['X initial (Km): ', num2str(h0)]);
+    display(['Theta (deg): ', num2str(theta.*180./(3.1415926))]);
     display(['Velocity (Km/s): ', num2str(vel)]);
 end
 
 function [m] = Weighted_Ballistic_Inversion(h, z, w)
-    W = diag((w.^(0.5)));
+    W = diag((w.^(-1.0)));
     G = ones(length(h), 3);
     G(:, 2) = G(:, 2) .* h;
     G(:, 3) = G(:, 3) .* (h .^ 2);
     m = pinv((G.')*(W.')*(W)*(G))*(G.')*(W.')*(W)*(z);
+end
+
+function [m] = Unweighted_Ballistic_Inversion(h, z)
+    Weighted_Ballistic_Inversion(h, z, ones(length(h), 1));
 end
