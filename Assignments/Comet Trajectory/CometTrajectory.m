e6061c4c2b77c5d6f4bd516b42f1da8050e6f068
Garrett Frank Sickles <garrettsickles@gmail.com>
2016-01-30 01:14:55 -0700

    Working solution now implemented... That was simple.

--- Assignments/Comet Trajectory/CometTrajectory.m
+++ Assignments/Comet Trajectory/CometTrajectory.m
@@ -6,9 +6,12 @@ function Comet_Trajectory(filename)
     y = data.data(:,2);
     angle = data.data(:,3);
     sigma = data.data(:,4);
-    
-    
-    % Draw the data points
+    Gx = ones(length(x), 2);
+    Gx(:,2) = cosd(angle);
+    mx = Least_Squares_Inversion(x, Gx, diag(sigma.^-2))
+    Gy = ones(length(y), 2);
+    Gy(:,2) = sind(angle);
+    my = Least_Squares_Inversion(y, Gy, diag(sigma.^-2))
     figure;
     scatter(x, y, 'b', 'o')
     hold on;
@@ -17,18 +20,11 @@ function Comet_Trajectory(filename)
        plot(x(i)+sigma(i)*cos(t), y(i)+sigma(i)*sin(t), 'k');
        hold on;
     end
+    plot(mx(1)+mx(2)*cos(t), my(1)+my(2)*sin(t), 'b');
     axis equal;
     
 end
 
-% This function take a model and performs a generalized least squares
-% inversion
-% --- Input Parameters ---
-% --- m: Model Vector
-% --- Output Parameters ---
-% --- d: Data Vector
-% --- G: The Magical Matrix Operator
-% --- W: Data Weighting Matrix
 function [ m ] = Least_Squares_Inversion(d, G, W)
     m = pinv((G.')*(W.')*(W)*(G))*(G.')*(W.')*(W)*(d);
 end
