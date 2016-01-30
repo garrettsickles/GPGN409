6ade295906c2f7df2e686d699f8f9d5cb56e18be
Garrett Frank Sickles <garrettsickles@gmail.com>
2016-01-30 02:03:28 -0700

    Added orbits of the Earth and Jupiter around the Sun

--- Assignments/Comet Trajectory/CometTrajectory.m
+++ Assignments/Comet Trajectory/CometTrajectory.m
@@ -1,26 +1,52 @@
-% This function takes a filename containing comet trajectory data in the
-% and plots the data as well as an inversion for the elliptical path
 function Comet_Trajectory(filename)
+    % Import the data
     data = importdata(filename);
-    x = data.data(:,1)
+    x = data.data(:,1);
     y = data.data(:,2);
     angle = data.data(:,3);
     sigma = data.data(:,4);
+    t = 0:0.001:(2*3.1415926);
+    % Setup and run the inversion
     Gx = ones(length(x), 2);
     Gx(:,2) = cosd(angle);
-    mx = Least_Squares_Inversion(x, Gx, diag(sigma.^-2))
+    mx = Least_Squares_Inversion(x, Gx, diag(sigma.^-2));
     Gy = ones(length(y), 2);
     Gy(:,2) = sind(angle);
-    my = Least_Squares_Inversion(y, Gy, diag(sigma.^-2))
+    my = Least_Squares_Inversion(y, Gy, diag(sigma.^-2));
+    % Plot the trajectory
     figure;
-    scatter(x, y, 'b', 'o')
+    plot(mx(1)+mx(2)*cos(t), my(1)+my(2)*sin(t), 'b');
     hold on;
-    t = 0:0.001:(2*3.1415926);
+    % Plot the data points
+    scatter(x,y,30,'MarkerEdgeColor','b',...
+        'MarkerFaceColor',[0 .5 .5],...
+        'LineWidth',1.5);
+    hold on;
+    % Plot the error
     for i=1:length(sigma)
-       plot(x(i)+sigma(i)*cos(t), y(i)+sigma(i)*sin(t), 'k');
+       plot(x(i)+sigma(i)*cos(t), y(i)+sigma(i)*sin(t),'k',...
+           'LineWidth',2.0);
        hold on;
     end
-    plot(mx(1)+mx(2)*cos(t), my(1)+my(2)*sin(t), 'b');
+    % Plot the sun
+    plot(0, 0,'-ko',...
+        'LineWidth',1,...
+        'MarkerEdgeColor',[1 .5 0],...
+        'MarkerFaceColor',[1 .5 0],...
+        'MarkerSize',10);
+    hold on;
+    % Plot the Earth's Orbit
+    plot(cos(t), sin(t),...
+        'Color', [0 0.5 0],...
+        'LineWidth',2);
+    % Plot Jupiter's Orbit
+    Jd = 5.20;
+    plot(Jd*cos(t), Jd*sin(t),...
+        'Color', [.9 .3 0],...
+        'LineWidth',2);
+    title('Assignment 1: Inverting for the Trajectory of a Comet');
+    xlabel('X (AU)');
+    ylabel('Y (AU)');
     axis equal;
     
 end
