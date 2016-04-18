c3a1b97cd7177699e6df48cab8caf32d92b9c1cf
Garrett Frank Sickles <garrettsickles@gmail.com>
2016-04-18 15:58:26 -0600

    Plotting is Working

--- Homework/HorizonInterpolation/HorizonInterpolation.m
+++ Homework/HorizonInterpolation/HorizonInterpolation.m
@@ -29,10 +29,10 @@ function HorizonInterpolation()
     z_model = ones(length(x), 1);
     r_d = zeros(iter, 1);
     r_m = zeros(iter, 1);
-    S_mv = logspace(-4,4,iter);
+    S_mv = logspace(-2,2,iter);
 
     for i=1:iter
-        W_m = W_mk./(x_delta*S_mv(i).^2);
+        W_m = W_mk./(S_mv(i));
         z(:,i) = pinv(K'*(W_d'*W_d)*K+W_m'*W_m)*(K'*(W_d'*W_d)*z_data+W_m'*W_m*z_model);
         r_d(i) = (norm(W_d*(K*z(:,i)-z_data)));
         r_m(i) = (norm(W_mk*(z(:,i)-z_model)));
@@ -41,7 +41,35 @@ function HorizonInterpolation()
     r_norm = (r_d.^2+r_m.^2).^(0.5);
     [junk, index] = min(r_norm);
     
-    figure('Position', [0, 0, 800, 300]);
+    figure;
+    subplot(2,2,1);
+    plot(r_m, r_d,...
+        'o', 'MarkerFaceColor', 'k', 'color', 'k'); hold on;
+    scatter(r_m(index), r_d(index),...
+        'o', 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g'); hold on;
+    scatter(r_m(1), r_d(1),...
+        'o', 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r'); hold on;
+    scatter(r_m(iter), r_d(iter),...
+        'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b'); hold on;
+    axis equal;
+    xlabel('||r_{m}||');
+    ylabel('||r_{d}||');
+    title('Data and Model Residual Comparison');
+    
+    subplot(2,2,2);
+    plot(log10(S_mv), sqrt(r_d.^2+r_m.^2),...
+        'o', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k'); hold on;
+    plot(log10(S_mv(index)), r_norm(index),...
+        'o', 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g'); hold on;
+    plot(log10(S_mv(1)), r_norm(1),...
+        'o', 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r'); hold on;
+    plot(log10(S_mv(iter)), r_norm(iter),...
+        'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b'); hold on;
+    xlabel('\sigma_{m}');
+    ylabel('$\sqrt{||r_{m}||^{2}+||r_{m}||^{2}}$','Interpreter','Latex');
+    title('Minimized Residual with respect to Uncertainty');
+    
+    subplot(2,2,[3,4]);
     errorbar(x_data, z_data, s_data, 'ro', 'color', 'k'); hold on;
     scatter(x, z_model, '.',...
         'MarkerEdgeColor','m',...
@@ -53,6 +81,8 @@ function HorizonInterpolation()
     ylabel('Z (km)');
     axis tight;
     axis equal;
+    legend('Data Points','Model','Min. Model','Max. Model','Opt. Model');
+    title('Raw Data with Certain Models Displayed');
     
     figure;
     [X, Y] = meshgrid(log10(S_mv), x);
@@ -61,29 +91,14 @@ function HorizonInterpolation()
     hold on;
     plot3(log10(S_mv(1)).*ones(length(x),1),x,z(:,1),...
         'Color', 'r', 'LineWidth', 4); hold on;
-    plot3(log10(S_mv(index)).*ones(length(x),1),x,z(:,index),...
-        'Color', 'g', 'LineWidth', 4); hold on;
     plot3(log10(S_mv(iter)).*ones(length(x),1),x,z(:,iter),...
         'Color', 'b', 'LineWidth', 4); hold on;
+    plot3(log10(S_mv(index)).*ones(length(x),1),x,z(:,index),...
+        'Color', 'g', 'LineWidth', 4); hold on;
     view([43, 54]);
-    
-    figure;
-    plot(r_m, r_d,...
-        'o', 'MarkerFaceColor', 'k', 'color', 'k'); hold on;
-    scatter(r_m(index), r_d(index),...
-        'o', 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g'); hold on;
-    scatter(r_m(1), r_d(1),...
-        'o', 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r'); hold on;
-    scatter(r_m(iter), r_d(iter),...
-        'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b'); hold on;
-    
-    figure;
-    plot(log10(S_mv), sqrt(r_d.^2+r_m.^2),...
-        'o', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k'); hold on;
-    plot(log10(S_mv(index)), r_norm(index),...
-        'o', 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g'); hold on;
-    plot(log10(S_mv(1)), r_norm(1),...
-        'o', 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r'); hold on;
-    plot(log10(S_mv(iter)), r_norm(iter),...
-        'o', 'MarkerFaceColor', 'b', 'MarkerEdgeColor', 'b'); hold on;    
+    title('Model Space with Specific Models');
+    xlabel('X (km)');
+    ylabel('Uncertainty');
+    zlabel('Z (km)');
+    legend('Model Space','Min. Model','Max. Model','Opt. Model');
 end
